2e84465d4d0a52942e73207f8e300e2803686b16
Khushboo Kaur <ms.khushboo13@gmail.com>
2016-09-30 21:52:39 +0530

    Implemented Method that retrieves list of archives

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ var server=app.listen(process.env.PORT || 5000, function () {
 app.get('/', function(req, res) {
 	console.log("\nLaunching client.html");
     
-	res.sendFile( __dirname + "/" + "views/public/screensharingtest.html" );	
+	res.sendFile( __dirname + "/" + "views/public/client.html" );	
 });
+//just replace client with screensharingtest.html in case of testing the screensharingtest
 
 //On Submitting the form, sessionId and Token are returned.
 app.get('/getSession', function (req, res) {
@@ -93,4 +94,33 @@ app.get('/archiveNotification',function(req, res){
 	}
 	//else
 		//console.log("GET METHOD YET TO BE IMPLEMENTED")
+})
+
+
+app.get('/getarchives',function(req, res){
+	if(req.method =="GET"){
+		console.log("\nRetrieving the Archive List...");
+		opentok.listArchives( function(error, archives, totalCount) {
+			if (error) 
+				return console.log("Got error while retrieving the list of archives:", error);
+
+			console.log("Total Count ="+totalCount + " archives");
+			console.log("Size of Archives="+archives.length);
+			console.log("Archives JSON="+archives);
+			
+			for (var i = 0; i < archives.length; i++) {
+				console.log("Archive Id="+archives[i].id+"   Archive name="+archives[i].name);
+				
+				opentok.getArchive(archives[i].id, function(err, archive) {
+					if (err) return console.log(err);
+
+					console.log(archive);
+				});
+			}
+		});
+		
+		res.end();		
+	}
+	else
+		console.log("You should use Get method for retrieving the archives");
 })
